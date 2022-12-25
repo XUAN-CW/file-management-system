4d913f828e95ba904c037fc6003abd1bd33702c5
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2022-12-25 20:54:20 +0800

    修改端口
    Update application.yaml

--- file-management-system-frontend/src/request/index.js
+++ file-management-system-frontend/src/request/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // 创建一个 axios 实例
 const service = axios.create({
-	baseURL: 'http://127.0.0.1:8080/', // 所有的请求地址前缀部分
+	baseURL: 'http://127.0.0.1:8081/', // 所有的请求地址前缀部分
 	timeout: 60000, // 请求超时时间毫秒
 	withCredentials: true, // 异步请求携带cookie
 	headers: {
