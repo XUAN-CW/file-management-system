1bd9f15242b06951fd2972aad83cce21e9a46a21
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2022-12-26 00:08:54 +0800

    Update HVideoPlayer.vue

--- file-management-system-frontend/src/components/HVideoPlayer.vue
+++ file-management-system-frontend/src/components/HVideoPlayer.vue
@@ -1,6 +1,6 @@
 <template>
   <div ref="videoPlayerDiv">
-    <video ref="videoPlayer" :height="videoPlayerHeight" preload="auto" controls :data-setup="dataSetup"
+    <video ref="videoPlayer"  preload="auto" controls :data-setup="dataSetup"
       class="video-js">
       <source :src="videoSrc">
     </video>
@@ -19,7 +19,7 @@ export default {
   data() {
     return {
       // videoPlayerWidth: 650,
-      videoPlayerHeight: 330,
+      // videoPlayerHeight: 330,
       dataSetup: '{"fluid": true}',
       isInViewport: false
     };
@@ -41,7 +41,7 @@ export default {
     this.player = player
 
     // this.videoPlayerWidth = this.$refs.videoPlayerDiv.offsetWidth
-    this.videoPlayerHeight = this.$refs.videoPlayerDiv.offsetHeight
+    // this.videoPlayerHeight = this.$refs.videoPlayerDiv.offsetHeight
 
     const observer = new IntersectionObserver(entries => {
       if (entries[0].isIntersecting) {
