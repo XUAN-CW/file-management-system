7c1b4fc57857d0c69aecccf83ede31fb1a2edb41
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2023-02-01 20:03:12 +0800

    Create DeleteEmptyDirectories.java

---
+++ src/test/java/com/xuanchengwei/filemanagementsystem/utils/filemanage/DeleteEmptyDirectories.java
@@ -0,0 +1,25 @@
+package com.xuanchengwei.filemanagementsystem.utils.filemanage;
+
+import java.io.File;
+
+public class DeleteEmptyDirectories {
+
+    public static void deleteEmptyDirectories(File directory) {
+        if (directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    deleteEmptyDirectories(file);
+                }
+            }
+            if (directory.list().length == 0) {
+                directory.delete();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        File rootDirectory = new File("D:\\qbit_download_complete\\qbit_download_check_5");
+        deleteEmptyDirectories(rootDirectory);
+    }
+}
