6af9681a0cd56a938fc862bc32c72af1bb46e019
xuanchengwei <xuanchengwei@coros.com>
2023-02-02 19:15:27 +0800

    Create DistinctFileTest.java

---
+++ src/test/java/com/xuanchengwei/filemanagementsystem/utils/filemanage/DistinctFileTest.java
@@ -0,0 +1,47 @@
+package com.xuanchengwei.filemanagementsystem.utils.filemanage;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.xuanchengwei.filemanagementsystem.entity.FileMetadata;
+import com.xuanchengwei.filemanagementsystem.mapper.FileMetadataMapper;
+import com.xuanchengwei.filemanagementsystem.utils.FileMetadataUtils;
+import com.xuanchengwei.filemanagementsystem.utils.TargetDirUtils;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author 禤成伟
+ * @date 2022-11-20 - 0:52
+ */
+@SpringBootTest
+public class DistinctFileTest {
+    @Autowired
+    FileMetadataMapper fileMetadataMapper;
+
+    @Test
+    public void FileDistinctUtil() throws IOException {
+        for (FileMetadata needDistinctFile : FileMetadataUtils.getFileMetadataList(new File("f"))) {
+
+            QueryWrapper<FileMetadata> wrapper = new QueryWrapper<>();
+            wrapper.eq("sha512",needDistinctFile.getSha512());
+            List<FileMetadata> existsFileMetadataList = fileMetadataMapper.selectList(wrapper);
+
+            for (FileMetadata existsFileMetadata : existsFileMetadataList) {
+                if (existsFileMetadata.getFile().exists()
+                        && !needDistinctFile.getFile().getAbsolutePath().equals(existsFileMetadata.getFile().getAbsolutePath())
+                ) {
+                    System.out.println(existsFileMetadata.getAbsolutePath() +"已存在\t" + needDistinctFile.getFile());
+                }
+            }
+        }
+    }
+
+}
