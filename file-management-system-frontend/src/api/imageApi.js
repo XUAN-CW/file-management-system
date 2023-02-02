dd276476448f5a92408917101b039e45e8c2f040
xuanchengwei <xuanchengwei@coros.com>
2023-02-02 15:58:36 +0800

    cd

--- file-management-system-frontend/src/api/imageApi.js
+++
@@ -1,9 +0,0 @@
-// 导入axios实例
-import httpRequest from '@/request/index'
-
-export function getImageList() {
-    return httpRequest({
-		url: '/image/getImageList',
-		method: 'get',
-	})
-}
--- file-management-system-frontend/src/api/videoApi.js
+++
@@ -1,9 +0,0 @@
-// 导入axios实例
-import httpRequest from '@/request/index'
-
-export function getVideoList() {
-    return httpRequest({
-		url: '/video/getVideoList',
-		method: 'get',
-	})
-}
