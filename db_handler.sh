8671e366f11e54a136248b1d8ad3f944a0c1732b
xuanchengwei <xuanchengwei@coros.com>
2023-02-02 12:43:11 +0800

    Update db_handler.sh

--- db_handler.sh
+++ db_handler.sh
@@ -1,3 +1,7 @@
 sqlite3 file-management-system.db ".dump" > dump.sql
 sqlite3 new_db.db < dump.sql
-sqlite3 file-management-system.db    "SELECT 'DROP TABLE '||name||';' FROM sqlite_master WHERE type='table' AND name LIKE '_%old%_';"
+sqlite3 file-management-system.db    "SELECT 'DROP TABLE '||name||';' FROM sqlite_master WHERE type='table' AND name LIKE '_%old%_';" > delete_old_table.sql
+
+rm -rf dump.sql new_db.db
+sqlite3 file-management-system.db ".dump" > dump.sql
+sqlite3 new_db.db < dump.sql
