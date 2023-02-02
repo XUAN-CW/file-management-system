7571acc996255f3ff28777c53ee6b4446d2bf623
xuanchengwei <xuanchengwei@coros.com>
2023-02-02 15:47:48 +0800

    Update DeleteEmptyDirectories.java

--- src/test/java/com/xuanchengwei/filemanagementsystem/utils/filemanage/DeleteEmptyDirectories.java
+++ src/test/java/com/xuanchengwei/filemanagementsystem/utils/filemanage/DeleteEmptyDirectories.java
@@ -1,10 +1,13 @@
 package com.xuanchengwei.filemanagementsystem.utils.filemanage;
 
+import com.xuanchengwei.filemanagementsystem.utils.FileMetadataUtils;
+
 import java.io.File;
+import java.io.IOException;
 
 public class DeleteEmptyDirectories {
 
-    public static void deleteEmptyDirectories(File directory) {
+    public static void deleteEmptyDirectories(File directory) throws IOException {
         if (directory.isDirectory()) {
             File[] files = directory.listFiles();
             if (files != null) {
@@ -13,13 +16,13 @@ public class DeleteEmptyDirectories {
                 }
             }
             System.out.println(directory.list().length + "  " +directory.getAbsolutePath());
-            if (directory.list().length == 0) {
+            if (directory.list().length == 0 || FileMetadataUtils.getFileList(directory).size() == 0) {
                 directory.delete();
             }
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File rootDirectory = new File("D:\\qbit_download_complete\\qbit_download_check_5");
         deleteEmptyDirectories(rootDirectory);
     }
