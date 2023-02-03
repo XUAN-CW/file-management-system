900c65226c3729cfdfe8e81bb4b351c0ff6e9249
xuanchengwei <xuanchengwei@coros.com>
2023-02-03 14:57:57 +0800

    Update docker_build.sh

--- hashutil/docker_build.sh
+++ hashutil/docker_build.sh
@@ -6,7 +6,8 @@ docker build -t hashutil:1.0 .
 
 docker run --rm \
   --name hashutil \
-  -v /Users/xuanchengwei/Desktop/test-file:/hash/path \
+  -v /Users/xuanchengwei/Desktop/test-file/20230203145655:/hash/path/20230203145655 \
+  -v /Users/xuanchengwei/Desktop/test-file/20230203145652:/hash/path/20230203145652 \
    hashutil:1.0
 
 
