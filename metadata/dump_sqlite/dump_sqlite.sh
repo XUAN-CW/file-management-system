5807bf24bd04f2389f090885cd1d64d99f220f47
xuanchengwei <xuanchengwei@coros.com>
2023-02-03 11:10:57 +0800

    Update dump_sqlite.sh

--- metadata/dump_sqlite/dump_sqlite.sh
+++ metadata/dump_sqlite/dump_sqlite.sh
@@ -2,6 +2,12 @@ maximum_rows=100
 
 for table_name in $(sqlite3 file-management-system.db ".tables"); do
    table_rows=$(sqlite3 file-management-system.db "SELECT count(*) FROM ${table_name};")
+  if [ $table_rows -eq 0 ]; then
+    echo "${table_name}没有数据，不导出"
+    continue
+  else
+    echo "${table_name} 导出 ${table_rows} 行"
+  fi
    sql_file_number=$(($table_rows / $maximum_rows))
    echo "${table_name}  ${table_rows} ${sql_file_number}"
   for i in $(seq 0 sql_file_number); do
