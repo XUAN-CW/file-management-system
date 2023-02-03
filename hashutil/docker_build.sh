8cea368dda2775212a8e56b2d538c810b6ef845b
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2023-02-03 20:48:46 +0800

    Update docker_build.sh

--- hashutil/docker_build.sh
+++ hashutil/docker_build.sh
@@ -4,7 +4,6 @@ docker build -t hashutil:1.0 .
 
 docker run --rm \
   --name hashutil \
-  -v /Users/xuanchengwei/Desktop/test-file/20230203155141:/hash/path/20230203155141 \
-  -v /Users/xuanchengwei/Desktop/test-file/20230203155151:/hash/path/20230203155151 \
+  -v /share/single1/16T:/hash/path/20230203155141 \
    hashutil:1.0
 
