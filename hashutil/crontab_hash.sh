fba93c03a7a631e388527f92a84675647de09723
xuanchengwei <xuanchengwei@coros.com>
2023-02-03 15:12:17 +0800

    存档

--- hashutil/crontab_hash.sh
+++ hashutil/crontab_hash.sh
@@ -1,4 +1,7 @@
 
+docker image rm hashutil:1.0
+docker build -t hashutil:1.0 .
+
 docker run --rm \
   --name hashutil \
   -v /Users/xuanchengwei/Desktop/test-file/20230203145655:/hash/path/20230203145655 \
