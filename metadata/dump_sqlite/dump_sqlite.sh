2ccaefc8119e0941c46bb7cf2ee33c51d6e7d735
xuanchengwei <xuanchengwei@coros.com>
2023-02-03 13:27:59 +0800

    cd

--- metadata/dump_sqlite/dump_sqlite.sh
+++ metadata/dump_sqlite/dump_sqlite.sh
@@ -10,6 +10,6 @@ for table_name in $(sqlite3 file-management-system.db ".tables"); do
   printf "%-30s 表导出 %-8s 行，共 %-4s 个文件\n" ${table_name} ${table_rows} $(($sql_file_number + 1))
   for i in $(seq 0 $sql_file_number); do
     offset=$((i * ${maximum_rows}))
-    sqlite3 file-management-system.db ".mode insert" "SELECT * FROM ${table_name} LIMIT ${maximum_rows} OFFSET ${offset};" > "${table_name}_${i}.sql"
+    sqlite3 file-management-system.db ${table_name} "SELECT * FROM ${table_name} LIMIT ${maximum_rows} OFFSET ${offset};" > "${table_name}_${i}.sql"
   done
 done
