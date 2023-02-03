9143f49abeaaaabaf08d3644d69a4b8fc919b154
xuanchengwei <xuanchengwei@coros.com>
2023-02-03 10:53:04 +0800

    cd

--- metadata/dump_sqlite/dump_sqlite.sh
+++ metadata/dump_sqlite/dump_sqlite.sh
@@ -1,5 +1,5 @@
 for table_name in $(sqlite3 file-management-system.db ".tables"); do
-  for i in $(seq 0 1000); do
+  for i in $(seq 0 1); do
     offset=$((i*1000000))
     limit=$((offset+1000000))
     sqlite3 file-management-system.db ".mode insert" ".once ${table_name}_${i}.sql" "SELECT * FROM ${table_name} LIMIT 1000000 OFFSET ${offset};"
