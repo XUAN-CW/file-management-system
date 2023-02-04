15e7b58e0e05596f78c9dd4060c45fd675d74247
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2023-02-04 18:07:02 +0800

    Create FileTagEnum.java

---
+++ common/src/main/java/com/xuanchengwei/filemanagementsystem/entity/FileTagEnum.java
@@ -0,0 +1,12 @@
+package com.xuanchengwei.filemanagementsystem.entity;
+
+/**
+ * @author 禤成伟
+ * @date 2023-02-04 - 18:06
+ */
+public enum FileTagEnum {
+    /**
+     * 手动分配
+     */
+    manual;
+}
