2cd488b6a8169385320e747229ab9162e2258d9f
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2023-02-04 13:48:03 +0800

    Update XyplorerTest.java

--- fms-utils/src/test/java/com/xuanchengwei/filemanagementsystem/utils/xyplorer/XyplorerTest.java
+++ fms-utils/src/test/java/com/xuanchengwei/filemanagementsystem/utils/xyplorer/XyplorerTest.java
@@ -46,7 +46,6 @@ public class XyplorerTest {
     @Test
     public void writeTest() throws IOException {
         readTest();
-        System.out.println(11);
         List<FileMetadata> fileMetadataList = FileMetadataUtils.getFileMetadataList(new File("Z:\\迅雷下载2"));
         List<DataInfo> dataInfoList =  new ArrayList<>();
         for (FileMetadata fileMetadata : fileMetadataList) {
