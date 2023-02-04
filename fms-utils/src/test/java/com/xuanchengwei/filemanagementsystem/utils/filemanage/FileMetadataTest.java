5d19b22ebe8ba39e426ace290a3748e90251aa13
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2023-02-04 13:19:32 +0800

    Update FileMetadataTest.java

--- fms-utils/src/test/java/com/xuanchengwei/filemanagementsystem/utils/filemanage/FileMetadataTest.java
+++ fms-utils/src/test/java/com/xuanchengwei/filemanagementsystem/utils/filemanage/FileMetadataTest.java
@@ -30,7 +30,7 @@ public class FileMetadataTest {
     public void fileMetadataUtil() throws IOException {
         List<FileMetadata> fileMetadataList = Collections.synchronizedList(new ArrayList<>(10000));
 
-        List<File> fileList = FileMetadataUtils.getFileList(new File("Z:\\迅雷下载2\\HEYZO_1934"));
+        List<File> fileList = FileMetadataUtils.getFileList(new File("Z:\\迅雷下载2"));
         fileList.stream().forEach(target -> {
             System.out.println(target.getAbsolutePath());
             try {
