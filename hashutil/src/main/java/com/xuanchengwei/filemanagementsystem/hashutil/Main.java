aa6fbb00c60606af69e70079f5a9e1e9df7e1417
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2023-02-04 11:18:38 +0800

    存档

---
+++ hashutil/src/main/java/com/xuanchengwei/filemanagementsystem/hashutil/Main.java
@@ -0,0 +1,20 @@
+package com.xuanchengwei.filemanagementsystem.hashutil;
+
+import com.xuanchengwei.filemanagementsystem.utils.FileMetadataUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FileMetadataUtils.getFileList(new File("/hash/path")).stream().parallel().forEach(target -> {
+            System.out.println(target.getAbsolutePath());
+            try {
+                FileMetadataUtils.fastHashing(target);
+            } catch (Exception e) {
+                System.err.println(target.getAbsolutePath());
+                e.printStackTrace();
+            }
+        });
+    }
+}
