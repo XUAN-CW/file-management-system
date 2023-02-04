e723dd026af5441a27c7d6f6d22f85bc69789d07
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2023-02-04 16:57:41 +0800

    Update docker_build.sh

--- hashutil/sh/docker_build.sh
+++ hashutil/sh/docker_build.sh
@@ -3,7 +3,7 @@ docker image rm hashutil:1.0
 docker build -t hashutil:1.0 .
 
 docker run --rm \
-  --name hashutil \
-  -v /share/single1/16T:/hash/path/20230203155141 \
+  --name hashutil2 \
+  -v /share/SINGLE2/8T:/hash/path \
    hashutil:1.0
 
