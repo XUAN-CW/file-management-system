1499cf0ed092c487ce64784a0be78a1ee09fbe7a
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2023-02-04 12:35:14 +0800

    Update FileMetadataTest.java

--- fms-utils/src/test/java/com/xuanchengwei/filemanagementsystem/utils/filemanage/FileMetadataTest.java
+++ fms-utils/src/test/java/com/xuanchengwei/filemanagementsystem/utils/filemanage/FileMetadataTest.java
@@ -33,13 +33,13 @@ public class FileMetadataTest {
         List<File> fileList = FileMetadataUtils.getFileList(new File("Z:\\迅雷下载2\\HEYZO_1934(1)"));
         fileList.stream().forEach(target -> {
             System.out.println(target.getAbsolutePath());
-//            try {
-//                FileMetadata fileMetadata =  FileMetadataUtils.fastHashing(target);
-////                fileMetadataList.add(fileMetadata);
-//            } catch (IOException e) {
-//                System.err.println(target.getAbsolutePath());
-//                e.printStackTrace();
-//            }
+            try {
+                FileMetadata fileMetadata =  FileMetadataUtils.fastHashing(target);
+//                fileMetadataList.add(fileMetadata);
+            } catch (IOException e) {
+                System.err.println(target.getAbsolutePath());
+                e.printStackTrace();
+            }
         });
 
 //        fileMetadataList.forEach(fileMetadata -> fileMetadataMapper.insert(fileMetadata));
