f9b115f7e01e2136968b6e6a59025451f61a0fe3
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2023-02-04 18:14:55 +0800

    cd

--- common/src/main/java/com/xuanchengwei/filemanagementsystem/entity/FileTagSourceEnum.java
+++ common/src/main/java/com/xuanchengwei/filemanagementsystem/entity/FileTagSourceEnum.java
@@ -4,7 +4,7 @@ package com.xuanchengwei.filemanagementsystem.entity;
  * @author 禤成伟
  * @date 2023-02-04 - 18:06
  */
-public enum FileTagEnum {
+public enum FileTagSourceEnum {
     /**
      * 手动分配
      */
---
+++ fms-utils/src/test/java/com/xuanchengwei/filemanagementsystem/utils/filemanage/FileTagTest.java
@@ -0,0 +1,41 @@
+package com.xuanchengwei.filemanagementsystem.utils.filemanage;
+
+import com.xuanchengwei.filemanagementsystem.entity.FileMetadata;
+import com.xuanchengwei.filemanagementsystem.entity.FileTag;
+import com.xuanchengwei.filemanagementsystem.entity.FileTagSourceEnum;
+import com.xuanchengwei.filemanagementsystem.mapper.FileTagMapper;
+import com.xuanchengwei.filemanagementsystem.utils.FileMetadataUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author 禤成伟
+ * @date 2023-02-04 - 18:09
+ */
+@SpringBootTest
+public class FileTagTest {
+
+    @Autowired
+    FileTagMapper fileTagMapper;
+
+    @Test
+    public void addTag() throws IOException {
+        String[] tagList = new String[]{"jav","test"};
+
+        List<FileMetadata> fileMetadataList = FileMetadataUtils.getFileMetadataList(new File("Z:\\迅雷下载2"));
+        for (FileMetadata fileMetadata : fileMetadataList) {
+            for (String tag : tagList) {
+                FileTag fileTag = new FileTag();
+                fileTag.setSha512(fileTag.getSha512());
+                fileTag.setTag(tag);
+                fileTag.setSource(FileTagSourceEnum.manual.name());
+            }
+        }
+    }
+
+}
