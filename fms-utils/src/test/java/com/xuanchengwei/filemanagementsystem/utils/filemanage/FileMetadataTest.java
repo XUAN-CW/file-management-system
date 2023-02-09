4039e41cfc3ae9db1b6ade03e224a9ceafd211bf
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2023-02-09 22:14:42 +0800

    Update FileMetadataTest.java

--- fms-utils/src/test/java/com/xuanchengwei/filemanagementsystem/utils/filemanage/FileMetadataTest.java
+++ fms-utils/src/test/java/com/xuanchengwei/filemanagementsystem/utils/filemanage/FileMetadataTest.java
@@ -30,7 +30,7 @@ public class FileMetadataTest {
     public void fileMetadataUtil() throws IOException {
         List<FileMetadata> fileMetadataList = Collections.synchronizedList(new ArrayList<>(10000));
 
-        List<File> fileList = FileMetadataUtils.getFileList(new File("Y:\\2T-2"));
+        List<File> fileList = FileMetadataUtils.getFileList(new File("R:\\筛选中\\115\\3D"));
         fileList.stream().parallel().forEach(target -> {
             System.out.println(target.getAbsolutePath());
             try {
